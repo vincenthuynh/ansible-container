28afc3b278d616bc9e4d4087709bb04cf1179255
vincenthuynh <vinc3nt.huynh@gmail.com>
2018-05-18 11:35:01 -0400

    Fix pip 10 parse_requirements
    https://stackoverflow.com/questions/25192794/no-module-named-pip-req

--- setup.py
+++ setup.py
@@ -7,7 +7,10 @@ import distutils.log
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 from setuptools.command.sdist import sdist as SDistCommand
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 import container
 
 class PlaybookAsTests(TestCommand):
